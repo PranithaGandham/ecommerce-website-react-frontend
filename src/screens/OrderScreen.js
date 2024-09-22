57b42128fdde81f7a01d817cd594261f3316dcfb
PranithaGandham <gpranitha2003@gmail.com>
2024-09-22 13:05:18 +0530

    navigate

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,27 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import React, { useState, useEffect ,useParams, useNavigate } from "react";
+
+/* REACT ROUTER */
+import { Link } from "react-router-dom";
+
+/* REACT BOOTSTRAP */
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+
+/* PAYPAL BUTTONS */
 import { PayPalButton } from "react-paypal-button-v2";
-import { useDispatch, useSelector } from "react-redux";
+
+/* COMPONENTS */
 import Message from "../components/Message";
 import Loader from "../components/Loader";
+
+/* REACT - REDUX */
+import { useDispatch, useSelector } from "react-redux";
+
+/* ACTION CREATORS */
 import {
   getOrderDetails,
   payOrder,
   deliverOrder,
 } from "../actions/orderActions";
+
+/* ACTION TYPES */
 import {
   ORDER_PAY_RESET,
   ORDER_DELIVER_RESET,
 } from "../constants/orderConstants";
 
-function OrderScreen() {
-  const { id: orderId } = useParams();
+function OrderScreen({ history, match }) {
+  const { orderId } = useParams(); // Get the orderId from the URL params
   const navigate = useNavigate();
+
   const dispatch = useDispatch();
 
   const [sdkReady, setSdkReady] = useState(false);
 
+  /* PULLING A PART OF STATE FROM THE ACTUAL STATE IN THE REDUX STORE */
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, error, loading } = orderDetails;
 
@@ -34,47 +50,55 @@ function OrderScreen() {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
-  useEffect(() => {
-    console.log("OrderID:", orderId); // Logging orderId for debugging
-
-    if (!userInfo) {
-      navigate("/login");
-    }
-
-    const addPayPalScript = () => {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "https://www.paypal.com/sdk/js?client-id=AYgflmsaM7ccNLPlKUiufIyw8-spOE4UuS5XyyTCvhzheA-1EUcZF9qGlgXBZaSKcP5BY0zTc9WgINKe";
-      script.async = true;
-      script.onload = () => {
-        setSdkReady(true);
-      };
-      document.body.appendChild(script);
-    };
-
-    if (orderId) {
-      if (!order || successPay || order._id !== Number(orderId) || successDeliver) {
-        dispatch({ type: ORDER_PAY_RESET });
-        dispatch({ type: ORDER_DELIVER_RESET });
-        dispatch(getOrderDetails(orderId));
-      } else if (!order.isPaid) {
-        if (!window.paypal) {
-          addPayPalScript();
-        } else {
-          setSdkReady(true);
-        }
-      }
-    } else {
-      console.error("OrderID is undefined");
-    }
-  }, [dispatch, order, orderId, successPay, successDeliver, navigate, userInfo]);
-
-  if (!loading && !error && order) {
+  // ITEMS PRICE GETS CALCULATED ONLY IF WE HAVE AN ORDER
+  if (!loading && !error) {
     order.itemsPrice = order.orderItems
       .reduce((acc, item) => acc + item.price * item.qty, 0)
       .toFixed(2);
   }
 
+  // PAYPAL BUTTONS
+  const addPayPalScript = () => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://www.paypal.com/sdk/js?client-id=AYgflmsaM7ccNLPlKUiufIyw8-spOE4UuS5XyyTCvhzheA-1EUcZF9qGlgXBZaSKcP5BY0zTc9WgINKe";
+    script.async = true;
+    script.onload = () => {
+      setSdkReady(true);
+    };
+    document.body.appendChild(script);
+  };
+
+  useEffect(() => {
+    // IS USER IS NOT LOGGED IN THEN REDIRECT TO LOGIN PAGE
+    if (!userInfo) {
+      history.push("/login");
+    }
+
+    // CHECK IF WE HAVE THE ORDER DETAILS, IF NOT DISPATCH AN ACTION TO GET THE ORDER DETAILS
+    if (
+      !order ||
+      successPay ||
+      order._id !== Number(orderId) ||
+      successDeliver
+    ) {
+      dispatch({ type: ORDER_PAY_RESET });
+
+      dispatch({ type: ORDER_DELIVER_RESET });
+
+      dispatch(getOrderDetails(orderId));
+    } else if (!order.isPaid) {
+      // ACTIVATING PAYPAL SCRIPTS
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
+      }
+    }
+  }, [dispatch, order, orderId, successPay, successDeliver, history, userInfo]);
+
+  /* HANDLERS */
   const successPaymentHandler = (paymentResult) => {
     dispatch(payOrder(orderId, paymentResult));
   };
@@ -83,19 +107,11 @@ function OrderScreen() {
     dispatch(deliverOrder(order));
   };
 
-  if (loading) {
-    return <Loader />;
-  }
-
-  if (error) {
-    return <Message variant="danger">{error}</Message>;
-  }
-
-  if (!order) {
-    return <Message variant="danger">Order not found</Message>;
-  }
-
-  return (
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
     <div>
       <h1>Order: {order._id}</h1>
       <Row>
@@ -103,22 +119,29 @@ function OrderScreen() {
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
+
               <p>
                 <strong>Name: {order.User.name}</strong>
               </p>
+
               <p>
                 <strong>Email: </strong>
                 <a href={`mailto:${order.User.email}`}>{order.User.email}</a>
               </p>
+
               <p>
                 <strong>Shipping Address: </strong>
                 {order.shippingAddress.address}, {order.shippingAddress.city},{" "}
                 {order.shippingAddress.postalCode},{" "}
                 {order.shippingAddress.country}
               </p>
+
               {order.isDeliver ? (
                 <Message variant="success">
-                  Delivered on {order.deliveredAt ? order.deliveredAt.substring(0, 10) : null}
+                  Delivered on{" "}
+                  {order.deliveredAt
+                    ? order.deliveredAt.substring(0, 10)
+                    : null}
                 </Message>
               ) : (
                 <Message variant="warning">Not Delivered</Message>
@@ -127,10 +150,12 @@ function OrderScreen() {
 
             <ListGroup.Item>
               <h2>Payment</h2>
+
               <p>
                 <strong>Payment Method: </strong>
                 {order.paymentMethod}
               </p>
+
               {order.isPaid ? (
                 <Message variant="success">
                   Paid on {order.paidAt ? order.paidAt.substring(0, 10) : null}
@@ -142,6 +167,7 @@ function OrderScreen() {
 
             <ListGroup.Item>
               <h2>Order Items</h2>
+
               {order.orderItems.length === 0 ? (
                 <Message variant="info">Order is empty</Message>
               ) : (
@@ -150,13 +176,23 @@ function OrderScreen() {
                     <ListGroup.Item key={index}>
                       <Row>
                         <Col md={1}>
-                          <Image src={item.image} alt={item.name} fluid rounded />
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
                         </Col>
+
                         <Col>
-                          <Link to={`/product/${item.product}`}>{item.name}</Link>
+                          <Link to={`/product${item.product}`}>
+                            {item.name}
+                          </Link>
                         </Col>
+
                         <Col md={4}>
-                          {item.qty} X ₹{item.price} = ₹{(item.qty * item.price).toFixed(2)}
+                          {item.qty} X ₹{item.price} = ₹
+                          {(item.qty * item.price).toFixed(2)}
                         </Col>
                       </Row>
                     </ListGroup.Item>
@@ -173,27 +209,35 @@ function OrderScreen() {
               <ListGroup.Item>
                 <h2>Order Summary</h2>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Items:</Col>
+
                   <Col>₹{order.itemsPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Shipping:</Col>
+
                   <Col>₹{order.shippingPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Tax:</Col>
+
                   <Col>₹{order.taxPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Total:</Col>
+
                   <Col>₹{order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
